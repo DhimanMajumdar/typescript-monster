f3bc2ae3f750c6d5317095f6bee3e1ba6a50f6a7
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-04-23 00:58:01 +0530

    annotations in ts done

---
+++ annotations.ts
@@ -0,0 +1,13 @@
+// Strings
+let myName: string = "Dhiman Majumdar";
+myName = "Kabir Roy";
+console.log(myName);
+
+// Numbers
+let favNumber: number = 7;
+favNumber += 2;
+console.log(favNumber);
+
+// Boolean
+let isHard: boolean = true;
+console.log(isHard);
