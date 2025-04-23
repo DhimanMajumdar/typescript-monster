fffb8a43246023e8acba8ca880afe57b1b8b9378
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-04-23 22:14:53 +0530

    enums done

---
+++ Enums.ts
@@ -0,0 +1,16 @@
+// enum WeatherConditions {
+//     Sunny,
+//     Cloudy,
+//     Rainy,
+//     Snowy,
+// }
+// console.log(WeatherConditions.Sunny) // 0
+
+enum WeatherConditionss {
+    Sunny = "sunny",
+    Cloudy = "cloudy",
+    Rainy = "rainy",
+    Snowy = "snowy",
+}
+
+console.log(WeatherConditionss.Sunny) // sunny
