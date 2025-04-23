0621f940a09f2b035358bbd05886f380acfbaaab
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-04-23 11:23:57 +0530

    Function Return Annotations

--- annotations.ts
+++ annotations.ts
@@ -26,3 +26,17 @@ console.log(result);
 const double = (x: number, y: number) => x * y;
 const res = double(2, 10);
 console.log(res);
+
+// Function Return Annotations
+
+// For Regular Function
+function double1(x: number): number {
+    return x * x;
+}
+const res1 = double1(2);
+console.log(res1);
+
+// For Arrow Function
+const double2 = (x: number): number => x * x;
+const res2 = double2(3);
+console.log(res2);
