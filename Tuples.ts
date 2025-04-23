b4916a0012a15a0bae60f1cbcb6b77870d7e6795
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-04-23 22:10:28 +0530

    tuples done

---
+++ Tuples.ts
@@ -0,0 +1,15 @@
+let myTuple: [string, number] = ["hello", 12];
+console.log(myTuple[0])
+console.log(myTuple[1])
+
+
+// Destructing Individual Elements
+
+let myTuplee: [string, number] = ["hello", 13]
+let [first, second] = myTuplee
+console.log(first);
+console.log(second);
+
+
+const productss: (number | string)[] = ["item1", 1];
+console.log(productss);
