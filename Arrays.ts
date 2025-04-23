e94c36548f15ca6fbf1559c34072c8e81d5f82da
DhimanMajumdar <dhimanmajumdar08233@gmail.com>
2025-04-23 14:33:02 +0530

    multidim array done

---
+++ Arrays.ts
@@ -0,0 +1,24 @@
+// Array Types
+
+// First way:
+
+const nums: number[] = [1, 2, 3, 4]
+const str: string[] = ["debojeet", "deewakar", "devanshi"];
+
+console.log(nums);
+console.log(str);
+
+// Second way:
+
+const nums2: Array<number> = [1, 2, 3, 4];
+const str2: Array<string> = ["Hi", "Bye", "Bhai"];
+
+console.log(nums2);
+console.log(str2);
+
+// Multidimensional Array types
+
+const nums3: number[][] = [[1, 2], [1, 3]];
+console.log(nums3)
+
+
